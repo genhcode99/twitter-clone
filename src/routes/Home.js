367b3b358c9db7b62427b99122f59492252851e9
JeUpark <genhcode99@gmail.com>
2021-06-27 13:56:48 +0900

    #3.0 Form and Database Setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
-  return <div>Home</div>
+  // 1. 버튼 클릭
+  const onSubmit = (event) => {
+    event.preventDefault()
+  }
+
+  // 2. Input Text 변경
+  const [tweet, setTweet] = useState('')
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event
+    setTweet(value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          value={tweet}
+          onChange={onChange}
+          placeholder="What's on your mind"
+          maxLength={120}
+        />
+        <input type='submit' value='Tweet' />
+      </form>
+    </div>
+  )
 }
 
 export default Home
