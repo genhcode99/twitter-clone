c1a6de63f92a0354013ce24624d7257ba2eea462
JeUpark <genhcode99@gmail.com>
2021-06-28 19:40:01 +0900

    #5.0 Get My Own Nweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-import { authService } from '../firebaseApp'
+import { authService, dbService } from '../firebaseApp'
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const history = useHistory()
+
+  // #. 로그아웃
   const onLogOutClick = () => {
     authService.signOut()
     history.push('/')
   }
+
+  // #. 사용자가 쓴 Tweet 가져오기
+  const getMyTweets = async () => {
+    const tweets = await dbService
+      .collection('tweets')
+      .where('creatorId', '==', userObj.uid)
+      .orderBy('createdAt', 'desc')
+      .get()
+    console.log(tweets.docs.map((doc) => doc.data()))
+  }
+
+  // Use Effect
+  useEffect(() => {
+    getMyTweets()
+  }, [])
+
+  // Return
   return <button onClick={onLogOutClick}>Log Out</button>
 }
 
