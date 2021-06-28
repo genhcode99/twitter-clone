a34c9643b1f09e4d122afbe919da3de29cb7602b
JeUpark <genhcode99@gmail.com>
2021-06-28 20:07:23 +0900

    #5.1 Update Profile

--- src/components/Router.js
+++ src/components/Router.js
@@ -8,7 +8,7 @@ import Navigation from './Navigation'
 const Router = ({ isLoggedIn, userObj }) => {
   return (
     <HashRouter>
-      {isLoggedIn ? <Navigation /> : null}
+      {isLoggedIn ? <Navigation userObj={userObj} /> : null}
       <Switch>
         {isLoggedIn ? (
           <>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { authService, dbService } from '../firebaseApp'
 
 const Profile = ({ userObj }) => {
-  const history = useHistory()
+  // State (상태관리)
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName)
 
   // #. 로그아웃
+  const history = useHistory()
   const onLogOutClick = () => {
     authService.signOut()
     history.push('/')
@@ -21,13 +23,44 @@ const Profile = ({ userObj }) => {
     console.log(tweets.docs.map((doc) => doc.data()))
   }
 
+  // #. displayName 변경
+  const onSubmit = async (event) => {
+    event.preventDefault()
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      })
+    }
+  }
+
+  // #. Input 글씨 실시간 변경
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event
+    setNewDisplayName(value)
+  }
+
   // Use Effect
   useEffect(() => {
     getMyTweets()
   }, [])
 
   // Return
-  return <button onClick={onLogOutClick}>Log Out</button>
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          placeholder='Display Name'
+          onChange={onChange}
+          value={newDisplayName}
+        />
+        <input type='submit' placeholder='Update Profile' />
+      </form>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  )
 }
 
 export default Profile
